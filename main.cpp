ffd74c6f7890a1a48a91d457ea50dc5c38756f71
Mantas Ragaišis <ragaisis.m@gmail.com>
2025-12-22 19:57:36 +0200

    Add files via upload
    Sukurti programos griaučiai (main()), įtrauktos bibliotekos, kurių  reikalauja užduotis.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream> // cin, cout
+#include <fstream>  // Failų įvedimui/išvedimui
+#include <string>   // String tipo kintamiesiems
+#include <map>      // Asociatyvus konteineris
+
+using namespace std;
+
+int main() {
+    string f;
+    cout << "Iveskite faila: ";
+    cin >> f;
+
+    cout << "Analize baigta\n";
+}
