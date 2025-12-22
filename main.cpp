5ce5b8294289816765ae080a6dd5c874a52c8592
Mantas Ragaišis <ragaisis.m@gmail.com>
2025-12-22 20:23:52 +0200

    Add files via upload
    Įgyvendinta pirma užduotis. Tam reikalingos dvi funkcijos (CleanWord() - žodžių valymui nuo skyrybos ženklų; ZodziuDazniai() - skaičiuoja kiekvieno žodžio dažnį tekste). Naudojama regex biblioteka, kurios pagalba valome ir randame žodžius (R"([A-Za-zĄČĘĖĮŠŲŪŽąčęėįšųūž-]+)" - visos UTF-8 raidės, brūkšnys tarp žodžių, be skyrybos ženklų).
    Sukurtas tekstas.txt failas, kuriame yra tekstas naudojamas testavimams. Ten yra virš 1000 žodžių ir šiek tiek URL.
    Rezultatai išvedami į zodziu_daznis.txt failą.

--- main.cpp
+++ main.cpp
@@ -2,13 +2,46 @@
 #include <fstream>  // Failų įvedimui/išvedimui
 #include <string>   // String tipo kintamiesiems
 #include <map>      // Asociatyvus konteineris
+#include <regex>    // 
+#include <vector>   //
+#include <sstream>  //
 
 using namespace std;
 
+// 1 UŽDUOTIS
+// Naikina skyrybos ženklus šalia žodžių (veikia su UTF-8)
+string CleanWord(const string& z) {
+    regex pattern(R"([A-Za-zĄČĘĖĮŠŲŪŽąčęėįšųūž-]+)");       // Regex pattern (žodžiams su UTF-8)
+    //regex pattern(R"([A-Za-z0-9ĄČĘĖĮŠŲŪŽąčęėįšųūž-]+)");  // jei norim, kad rodytų ir skaičius
+
+    smatch m; // Regex match objektas
+    if (regex_search(z, m, pattern)) return m.str();
+    return "";
+}
+
+// Žodžių skaičiavimas
+map<string,int> ZodziuDazniai(const string& failas) {
+    map<string,int> m;
+    ifstream fin(failas);
+    string zodis;
+    while (fin >> zodis) {
+        string val = CleanWord(zodis);
+        if (!val.empty()) m[val]++;
+    }
+    return m;
+}
+
 int main() {
-    string f;
+    string failas;
     cout << "Iveskite faila: ";
-    cin >> f;
+    cin >> failas;
+
+    auto dazniai = ZodziuDazniai(failas);
+
+    ofstream out("zodziu_daznis.txt");
+    for(auto &p : dazniai)
+        if(p.second > 1)
+            out << p.first << " " << p.second << "\n";
 
     cout << "Analize baigta\n";
 }
